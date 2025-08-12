5b19fe3975cc566c5fb9f4859c7b5839fa7fe4a6
sharpslice <davidlein1440p@gmail.com>
2025-08-12 16:13:47 -0700

    feat wip: add google oAuth with api and credentials set up

--- backend/routes/auth/google.ts
+++ backend/routes/auth/google.ts
@@ -1,8 +1,24 @@
 import express from 'express';
-
+import passport from 'passport'
+import {Strategy as GoogleStrategy} from 'passport-google-oauth20';
 const google = express.Router();
 
+
+passport.use(new GoogleStrategy({
+    clientID:process.env.GOOGLE_CLIENT_ID ?? "",
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ??"",
+    callbackURL: "http://localhost:3000/auth/google/callback"
+
+
+},async(accessToken, refreshToken, profile,done)=>{
+
+
+   
+}),)
+
+
+
 google.get('/',(req,res)=>{
-    
+
 })
 export default google;
