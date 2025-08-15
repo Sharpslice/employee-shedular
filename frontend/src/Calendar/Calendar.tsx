9063a0a80ceffba4039f5a61f2cf82cf801ed2bb
sharpslice <davidlein1440p@gmail.com>
2025-08-15 00:31:10 -0700

    feat: add correct days to current month on calendar

--- frontend/src/Calendar/Calendar.tsx
+++ frontend/src/Calendar/Calendar.tsx
@@ -1,28 +1,36 @@
 
 import { useEffect } from "react"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import {getDaysInMonth} from 'date-fns';
+import {getDaysInMonth,format, startOfMonth, getDay} from 'date-fns';
 import { Box } from '@mui/material';
 
 function Calendar(){
 
 
-    //const today = new Date();
-    //const totalDays = getDaysInMonth(today);
+    const today = new Date();
+    const totalDays = getDaysInMonth(today);
+
+    const dayIndex = getDay(startOfMonth(today));
 
     useEffect(()=>{
-        
+        console.log(totalDays)
     })
 
-    const daysInAMonth = Array.from({length: 35},()=>{
-        return {test:1}
+    const daysInAMonth: (number | null)[] = Array.from({length:42},()=>{
+        return null
     })
+    for(let day = dayIndex,i=1; i<=totalDays;day++,i++){
+        daysInAMonth[day] = i;
+    }
+
+
+    
 
     const daysOfTheWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
     return (
     <>
-    <Box sx={{display:'grid',gridTemplateColumns:'repeat(7,1fr)',gridTemplateRows:'50px repeat(5,1fr)',width:'65vw',height:'100vh'}}>
+    <Box sx={{display:'grid',gridTemplateColumns:'repeat(7,1fr)',gridTemplateRows:'50px repeat(6,1fr)',width:'65vw',height:'100vh'}}>
 
         {daysOfTheWeek.map(days =>(
             <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',border:'1px solid grey',height:'50px'}}>
@@ -32,12 +40,12 @@ function Calendar(){
 
         ))}
 
-            {daysInAMonth.map((day) =>(
-                
-                <Box sx={{width:'100%', height:'100%',border:'1px solid grey',borderRadius:'0'}}>
-                    {day.test}
-                </Box>
-            ))}
+        {daysInAMonth.map((day) =>(
+            
+            <Box sx={{width:'100%', height:'100%',border:'1px solid grey',borderRadius:'0'}}>
+                {day}
+            </Box>
+        ))}
         
     </Box>
         
