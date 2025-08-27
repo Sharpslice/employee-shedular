0cc41524adb9702a4141b52ec43f0cf7a1228fd8
sharpslice <davidlein1440p@gmail.com>
2025-08-26 21:10:24 -0700

    feat: header nav links to schedule,contacts,and availability

--- frontend/src/Dashboard/Dashboard.tsx
+++ frontend/src/Dashboard/Dashboard.tsx
@@ -3,17 +3,29 @@ import { useEffect, useState } from "react"
 import {DateTime} from 'luxon'
 import axios from "axios";
 import Header from "./Header";
+import { Box, Container, Group } from "@mantine/core";
 
-
-
+type DayElement ={
+    week:number,
+    date: string,
+    days_of_week:number,
+    day_of_month:number
+    month: number
+}
+type CalendarResponse ={
+    dateArray: DayElement[]
+   
+    
+}
 function Dashboard(){   
     const [dateValue,setDateValue]= useState<string | null>(DateTime.local().toISODate())
     const [view,setView] = useState<'day' | 'week' | 'bi-week' | 'month'>('week')
 
+    const [datesRange,setDatesRange] = useState<DayElement[]>([]) 
     useEffect(()=>{
         const fetchData =async() =>{
-            const response = await axios.get(`http://localhost:3000/api/calendar/date?date=${dateValue}&view=${view}`)
-
+            const response = await axios.get<CalendarResponse>(`http://localhost:3000/api/calendar/date?date=${dateValue}&view=${view}`)
+            setDatesRange(response.data.dateArray)
             console.log(response.data.dateArray)
         }
         fetchData()
@@ -22,7 +34,31 @@ function Dashboard(){
     return(
         <>  
             <Header dateValue={dateValue} setDateValue={setDateValue} view={view} setView={setView}/>
+            <Container style={{backgroundColor:'lightgray',width:'100vw', height:'100vh'}} size='100%' >
+
+               
+            <Group>
+
             
+                {datesRange.map((day)=>{
+                    
+                    day.date = DateTime.fromISO(day.date).toISODate()!
+                    return (
+                        <Box style={{border:'1px solid black'}}>
+                            {day.day_of_month}
+                        </Box>
+
+                        
+                    )
+                })}
+            </Group>    
+                
+               
+
+
+
+
+            </Container>
             
             
 
--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -1,15 +1,40 @@
-import { Box } from '@mantine/core';
+import { Container, Group } from '@mantine/core';
+import { Link } from 'react-router-dom';
+
+
+type Links={
+    link:string,
+    label:string
+}
 
 function Header(){
+    const links:Links[] = [
+        {link: '/schedule', label: 'Schedule'},
+        {link: '/contacts', label: 'Contacts'},
+        {link: '/availability', label:'Availability'}
+    ]
+
+    const items = links.map((link)=>{
+        return (
+            <Link key={link.label} to={link.link} >
+                {link.label}
+            </Link>
+        )
+       
+    })
+
+
     return(<>
-        <Box style={{width:'100%',height:'4rem', backgroundColor:'grey'}}>
-            
+        <Container fluid style={{width:'100%',height:'4rem', backgroundColor:'grey'}}>
+            <Group>
+                {items}
+            </Group>
 
 
 
 
 
-        </Box>
+        </Container>
     </>)
 }   
 
