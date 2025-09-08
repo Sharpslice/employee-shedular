25e81abe379fa744cdf049ad77b355e01013a6b9
sharpslice <davidlein1440p@gmail.com>
2025-09-08 11:12:45 -0700

    refactor: removed employeeboard temporarily

--- frontend/src/Dashboard/Cell/Shift/ShiftCell.tsx
+++ frontend/src/Dashboard/Cell/Shift/ShiftCell.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@mantine/core";
+import { Flex, Text } from "@mantine/core";
 import type { Shift } from "../../Interfaces/Shift";
 import {DateTime} from 'luxon'
 interface PlaceholderProp{
