6312efc6efed63cfdf8b5a3280d6f4585fa6245d
sharpslice <davidlein1440p@gmail.com>
2025-09-08 19:25:05 -0700

    feat(day-view): the time in header are now internally utc time dated and will display as hour am/pm

--- frontend/src/Dashboard/views/DayView.tsx
+++ frontend/src/Dashboard/views/DayView.tsx
@@ -1,8 +1,9 @@
-import { Box, Group } from "@mantine/core"
+import { Box, Flex, Group } from "@mantine/core"
 
 import { useOutletContext } from "react-router-dom"
 import type { Shift } from "../Interfaces/Shift"
 import type { Employee } from "../Interfaces/Employee"
+import { DateTime } from "luxon"
 type DayElement ={
     week:number,
     date: string,
@@ -17,19 +18,25 @@ type DayElement ={
 function DayView(){
     const { dateRange,shifts,employeeList } = useOutletContext<{ dateRange: DayElement[],shifts:Map<number,Shift[]>, employeeList:Employee[] }>();
     
-    const workHours = ['9 am', '10 am','11 am', '12 pm' ,'1 am', '2 am', '3 am', '4 am', '5 am']
+    const workHours = ['09:00:00', '10:00:00','11:00:00', '12:00:00' ,'13:00:00', '14:00:00', '15:00:00', '16:00:00', '17:00:00']
     console.log(shifts)
+
+    const convertTo12hr = (time:string | null)=>{
+        if(!time) return ""
+        const dt = DateTime.fromFormat(time,'HH:mm:ss');
+        return dt.toFormat('h a')
+    }
     return(
     <>
         <Box display={'grid'} w={'100%'} style={{gridTemplateColumns:'repeat(9,1fr)'}}>
             {workHours.map((time)=>{
                 return (
-                    <Box key={time}>{time}</Box>
+                    <Box key={time}>{convertTo12hr(time)}</Box>
             
                 )
             })}
            
-
+            
 
         </Box>    
             
