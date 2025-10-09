0cb577b8c3b77cc96327f5b0dc2e52d4e2c798c6
sharpslice <davidlein1440p@gmail.com>
2025-10-09 00:16:39 -0700

    fix(day-view): day view now works with new slot system

--- frontend/src/Dashboard/views/DayView/DayView.tsx
+++ frontend/src/Dashboard/views/DayView/DayView.tsx
@@ -74,7 +74,7 @@ function DayView(){
                 
                 return(
                     todayShift ? 
-                        <EmployeeRow employee={employee}>
+                        <EmployeeRow employee={employee} dateRange={dateRange}>
                             <Flex bg={'blue'} bd={'1px solid black'} pos={'relative'} left={`${((startTime!-9)/9)*100}%`} w={`${((endTime!-startTime!)/9)*100}%`}
                             style={{padding:10}}
                             
